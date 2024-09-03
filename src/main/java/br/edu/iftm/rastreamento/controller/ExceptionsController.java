8981c1e24cc621facffa1f42c44ecc5d42a3ec6b
matuzalembatista <matuzalem_pereira@hotmail.com>
2024-09-02 22:54:45 -0300

    adicionando ExceptionsController

--- src/main/java/br/edu/iftm/rastreamento/controller/ExceptionsController.java
+++ src/main/java/br/edu/iftm/rastreamento/controller/ExceptionsController.java
@@ -1,18 +1,19 @@
-package br.edu.iftm.rastreamento.controller;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import br.edu.iftm.rastreamento.service.exceptions.IdNaoEncontradoException;
 import br.edu.iftm.rastreamento.service.exceptions.NaoAcheiException;
 
 @ControllerAdvice
 public class ExceptionsController {
 
-	@ExceptionHandler(NaoAcheiException.class)
-	public ResponseEntity<?> naoAchei(NaoAcheiException e) {
-		System.out.println("------------------------");
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-	}
-}
+    @ExceptionHandler(NaoAcheiException.class)
+    public ResponseEntity<?> naoAchei(NaoAcheiException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+	
+    @ExceptionHandler(IdNaoEncontradoException.class)
+    public ResponseEntity<?> idNaoEncontrado(IdNaoEncontradoException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
